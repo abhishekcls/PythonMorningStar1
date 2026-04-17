de45978b502c3bc7d6db2356be7c653fefb1337d
Abhishek Samanta <Abhishek@example.com>
2026-04-17 13:46:07 +0530

    Day2

---
+++ lambda.py
@@ -0,0 +1,19 @@
+'''
+def add(a,b):
+    return a+b
+
+total=add(2,3)
+print(total)
+'''
+
+add=lambda a,b:a+b
+total=add(2,3)
+print(total)
+
+#Task1: convert the below square fx into lambda
+# def square(a):
+#     return a*a
+
+square=lambda a:a*a
+
+print(square(5))
---
+++ stringmanip.py
@@ -0,0 +1,9 @@
+name="abhishek samanta"
+print(name)
+print(name.title())
+print(name.upper())
+print(name.count("h"))
+print(name.isdigit())
+
+num="123"
+print(num.isdigit())
